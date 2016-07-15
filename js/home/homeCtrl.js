31e4a28692a859c39bb4ac1905dfbac51cef40f3
Candice Humpherys <smrtsmrf@gmail.com>
2016-07-14 23:05:49 -0600

    fixed controller

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -1,11 +1,11 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('homeCtrl', function ($scope, homeService, allTeamData) {
+app.controller('homeCtrl', function ($scope, homeService) {
 	$scope.teams = [
 		{team: 'Los Angeles Lakers', logo: 'images/lakers-logo.png'},
     		{team: 'Utah Jazz', logo: 'images/lakers-logo.png'},
     		{team: 'Miami Heat', logo: 'images/heat-logo.png'}
   	]
 
-  	$scope.allTeamData = allTeamData;
+  	$scope.allTeamData = homeService.getAllTeamData();
 });
